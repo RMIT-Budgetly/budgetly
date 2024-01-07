009b78355fd86baa542a6c7f0d1ef0a97c033cc6
Nhat Minh Phan <s3978598@rmit.edu.vn>
2024-01-07 15:45:24 +0700

    Update tracking selection UI
    Make the buttons appear in 1 column
    Simplify the text
    Add icon for each button

--- lib/components/tracking_selection.dart
+++ lib/components/tracking_selection.dart
@@ -10,67 +10,46 @@ class TrackingSelection extends StatelessWidget {
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       elevation: 8,
       child: Container(
-          padding: const EdgeInsets.all(16),
-          margin: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'Select Tracking',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+        padding: const EdgeInsets.symmetric(
+            vertical: 20, horizontal: 16), // Updated padding
+        margin: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Add New',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  createButton(
-                    "",
-                    'Add Transactions',
-                    () {
-                      Navigator.pushNamed(context, '/add_expenses');
-                    },
-                  ),
-                  // createButton(
-                  //   const Icon(Icons.add),
-                  //   'Add Income',
-                  //   () {
-                  //     Navigator.pushNamed(context, '/add_income');
-                  //   },
-                  // ),
-                  // createButton(
-                  //   const Icon(Icons.add),
-                  //   'Add Saving Goal',
-                  //   () {
-                  //     Navigator.pushNamed(context, '/add_goal');
-                  //   },
-                  // ),
-                ],
-              ),
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  createButton(
-                    "",
-                    'Add Weekly Plan',
-                    () {
-                      Navigator.pushNamed(context, '/weekly_plan_form');
-                    },
-                  ),
-                  createButton(
-                    "",
-                    'Add Saving Goal',
-                    () {
-                      Navigator.pushNamed(context, '/add_goal');
-                    },
-                  ),
-                ],
-              ),
-            ],
-          )),
+            ),
+            const SizedBox(height: 20),
+            createButton(
+              "assets/icons/transactions.svg",
+              'Transaction',
+              () {
+                Navigator.pushNamed(context, '/add_expenses');
+              },
+            ),
+            const SizedBox(height: 20),
+            createButton(
+              "assets/icons/weekly_plan.svg",
+              'Weekly Plan',
+              () {
+                Navigator.pushNamed(context, '/weekly_plan_form');
+              },
+            ),
+            const SizedBox(height: 20),
+            createButton(
+              "assets/icons/saving_goal.svg",
+              'Saving Goal',
+              () {
+                Navigator.pushNamed(context, '/add_goal');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -79,31 +58,39 @@ class TrackingSelection extends StatelessWidget {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          // border: Border.all(color: Colors.grey),
           borderRadius: BorderRadius.circular(10),
           color: Colors.white,
           boxShadow: [
             BoxShadow(
               color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 1,
-              blurRadius: 7,
-              offset: const Offset(0, 3), // changes position of shadow
+              spreadRadius: 2,
+              blurRadius: 6,
+              offset: const Offset(0, 3),
             ),
           ],
         ),
-        width: 110,
-        padding: const EdgeInsets.all(8),
-        margin: const EdgeInsets.only(left: 2, right: 2),
-        child: Flexible(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SvgPicture.asset(imagePath, width: 50),
-              const SizedBox(height: 10),
-              Text(text, textAlign: TextAlign.center),
-            ],
-          ),
+        width: 140,
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              imagePath,
+              width: 50,
+              height: 50,
+              color: const Color(0xFF007AFF), // Customize the SVG icon color
+            ),
+            const SizedBox(height: 10),
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
