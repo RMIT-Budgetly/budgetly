635aa3ebca0feb2a124394eea524fe33065a8f7b
Nhat Minh Phan <s3978598@rmit.edu.vn>
2024-01-09 09:22:28 +0700

    Done Login UI

--- lib/screens/login_page/login_page.dart
+++ lib/screens/login_page/login_page.dart
@@ -6,7 +6,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:personal_finance/widgets/image_picker.dart';
+import 'package:personal_finance/constants/style.dart';
 
 final _firebase = FirebaseAuth.instance;
 final _googleSignIn = GoogleSignIn();
@@ -123,21 +125,25 @@ class _AuthScreenState extends State<AuthScreen> {
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
               child: Column(
                 children: [
-                  Image.asset(
-                    'assets/images/signin_banner.png',
-                    fit: BoxFit.fitWidth,
+                  SvgPicture.asset(
+                    'assets/logo/budgetly_logo.svg', // Path to your SVG file
+                    width: 200,
+                    colorFilter:
+                        const ColorFilter.mode(Colors.black, BlendMode.srcIn),
                   ),
                   const SizedBox(
                     height: 10,
                   ),
                   Container(
                     alignment: Alignment.center,
-                    child: Text(
-                      'BUDGETLY',
+                    child: const Text(
+                      'Your Personal Finance Assistant',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
-                        color: Theme.of(context).colorScheme.primary,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                        color: primaryPurple,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.italic,
                       ),
                     ),
                   ),
@@ -154,22 +160,30 @@ class _AuthScreenState extends State<AuthScreen> {
                             decoration: InputDecoration(
                               prefixIcon: Icon(
                                 Icons.mail,
-                                color: Theme.of(context).colorScheme.primary,
+                                size: 22,
+                                color: Color.fromRGBO(
+                                    0, 0, 0, 0.5), // RGBA color for the icon
                               ),
                               labelText: 'Email Address',
                               labelStyle: TextStyle(
-                                  color: Theme.of(context).colorScheme.primary),
+                                color: Color.fromRGBO(
+                                    0, 0, 0, 0.5), // RGBA color for the text
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16, // Font weight
+                              ),
                               border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30.0),
+                                borderRadius: BorderRadius.circular(
+                                    12.0), // Border radius
                                 borderSide: const BorderSide(
-                                  width: 2,
+                                  width: 1,
                                   style: BorderStyle.solid,
                                 ),
                               ),
                               enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30.0),
+                                borderRadius: BorderRadius.circular(
+                                    12.0), // Border radius
                                 borderSide: BorderSide(
-                                  width: 2,
+                                  width: 1,
                                   style: BorderStyle.solid,
                                   color: Theme.of(context).colorScheme.primary,
                                 ),
@@ -190,29 +204,35 @@ class _AuthScreenState extends State<AuthScreen> {
                               _enteredEmail = value!;
                             },
                           ),
-                          const SizedBox(
-                            height: 10,
-                          ),
+                          const SizedBox(height: 10),
                           TextFormField(
                             decoration: InputDecoration(
                               prefixIcon: Icon(
                                 Icons.lock,
-                                color: Theme.of(context).colorScheme.primary,
+                                size: 22,
+                                color: Color.fromRGBO(
+                                    0, 0, 0, 0.5), // RGBA color for the icon
                               ),
                               labelText: 'Password',
                               labelStyle: TextStyle(
-                                  color: Theme.of(context).colorScheme.primary),
+                                color: Color.fromRGBO(
+                                    0, 0, 0, 0.5), // RGBA color for the text
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16, // Font weight
+                              ),
                               border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30.0),
+                                borderRadius: BorderRadius.circular(
+                                    12.0), // Border radius
                                 borderSide: const BorderSide(
-                                  width: 2,
+                                  width: 1,
                                   style: BorderStyle.solid,
                                 ),
                               ),
                               enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30.0),
+                                borderRadius: BorderRadius.circular(
+                                    12.0), // Border radius
                                 borderSide: BorderSide(
-                                  width: 2,
+                                  width: 1,
                                   style: BorderStyle.solid,
                                   color: Theme.of(context).colorScheme.primary,
                                 ),
@@ -233,6 +253,9 @@ class _AuthScreenState extends State<AuthScreen> {
                       ),
                     ),
                   ),
+                  const SizedBox(
+                    height: 10,
+                  ),
                   Container(
                     width: MediaQuery.of(context).size.width,
                     height: 35,
@@ -241,32 +264,46 @@ class _AuthScreenState extends State<AuthScreen> {
                       child: Text(
                         "Forgot Password?",
                         style: TextStyle(
-                            color: Theme.of(context).colorScheme.primary),
+                            color: black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            ),
                         textAlign: TextAlign.right,
                       ),
                       onPressed: () => {},
                     ),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 50,
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(90)),
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateColor.resolveWith(
-                            (states) => Theme.of(context).colorScheme.primary),
-                      ),
-                      onPressed: _submit,
-                      child: const Text(
-                        'Sign In',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
+                  const SizedBox(
+                    height: 10,
                   ),
+                  Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(90)),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(primaryPurple),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  12), // Set border radius to 12
+                            ),
+                          ),
+                        ),
+                        onPressed:
+                            _submit, // Replace with your actual onPressed function
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )),
                   const SizedBox(
                     height: 10,
                   ),
@@ -275,7 +312,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       Expanded(child: Divider()),
                       Text(
                         ' or ',
-                        style: TextStyle(fontSize: 14),
+                        style: TextStyle(fontSize: 16),
                       ),
                       Expanded(child: Divider()),
                     ],
@@ -287,15 +324,30 @@ class _AuthScreenState extends State<AuthScreen> {
                     width: MediaQuery.of(context).size.width,
                     height: 50,
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(90),
-                        border: Border.all(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 2,
-                        )),
+                      borderRadius:
+                          BorderRadius.circular(12), // Set border radius to 12
+                      color: Colors.white, // Set background color to white
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2), // Shadow color
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                          offset: Offset(0, 2), // Changes position of shadow
+                        ),
+                      ],
+                    ),
                     child: ElevatedButton(
                       style: ButtonStyle(
-                        backgroundColor: MaterialStateColor.resolveWith(
-                            (states) => Colors.white),
+                        backgroundColor: MaterialStateProperty.all(
+                            Colors.white), // Button background color
+                        elevation:
+                            MaterialStateProperty.all(0), // Remove elevation
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                12), // Same border radius as the container
+                          ),
+                        ),
                       ),
                       onPressed: signInWithGoogle,
                       child: Row(
@@ -311,7 +363,7 @@ class _AuthScreenState extends State<AuthScreen> {
                             style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 16,
-                                fontWeight: FontWeight.bold),
+                                fontWeight: FontWeight.w600),
                           ),
                         ],
                       ),
@@ -326,11 +378,14 @@ class _AuthScreenState extends State<AuthScreen> {
                       Text(
                         "Don't have account ?",
                         style: TextStyle(
-                          color: Theme.of(context).colorScheme.primary,
+                          color: black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
                         ),
                       ),
                       const SizedBox(
-                        width: 20,
+                        width: 15,
                       ),
                       GestureDetector(
                         onTap: () {
@@ -341,8 +396,9 @@ class _AuthScreenState extends State<AuthScreen> {
                         child: Text(
                           'Sign Up',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).colorScheme.primary,
+                            color: black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
                           ),
                         ),
                       ),
