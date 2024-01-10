efe566914a0ba8d6f62478b5a418566de5339d85
Nhat Minh Phan <s3978598@rmit.edu.vn>
2024-01-10 11:02:11 +0700

    Monthly Breakdown for This and Last Month

--- lib/screens/data_visualization/monthly_breakdown.dart
+++ lib/screens/data_visualization/monthly_breakdown.dart
@@ -13,22 +13,45 @@ class DataVisualizationPage extends StatefulWidget {
   State<DataVisualizationPage> createState() => _DataVisualizationPageState();
 }
 
-class _DataVisualizationPageState extends State<DataVisualizationPage> {
+class _DataVisualizationPageState extends State<DataVisualizationPage>
+    with TickerProviderStateMixin {
   final user = FirebaseAuth.instance.currentUser;
 
   late Map<String, Color> categoryColors;
+  late TabController _tabController;
 
-  Stream<Map<String, double>> streamExpenses() {
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+        length: 2, vsync: this); // 'this' refers to TickerProviderStateMixin
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  Stream<Map<String, double>> streamExpenses(bool isCurrentMonth) {
     var now = DateTime.now();
-    var firstDayLastMonth = DateTime(now.year, now.month - 1, 1);
-    var lastDayLastMonth = DateTime(now.year, now.month, 0);
+    var firstDay;
+    var lastDay;
+
+    if (isCurrentMonth) {
+      firstDay = DateTime(now.year, now.month, 1);
+      lastDay = DateTime(now.year, now.month + 1, 0);
+    } else {
+      firstDay = DateTime(now.year, now.month - 1, 1);
+      lastDay = DateTime(now.year, now.month, 0);
+    }
 
     return FirebaseFirestore.instance
         .collection('users')
         .doc(user!.uid)
         .collection('expenses')
-        .where('selectedDate', isGreaterThanOrEqualTo: firstDayLastMonth)
-        .where('selectedDate', isLessThanOrEqualTo: lastDayLastMonth)
+        .where('selectedDate', isGreaterThanOrEqualTo: firstDay)
+        .where('selectedDate', isLessThanOrEqualTo: lastDay)
         .snapshots()
         .map((snapshot) => snapshot.docs.take(snapshot.docs.length))
         .asyncMap((docs) async {
@@ -63,12 +86,14 @@ class _DataVisualizationPageState extends State<DataVisualizationPage> {
 
   // Create a list of colors for the chart
   final List<Color> chartColors = [
-    const Color(0xFF4F42FF),
-    const Color(0xFFEE00C7),
-    const Color(0xFFFF1F88),
-    const Color(0xFFFF7D57),
-    const Color(0xFFFFC146),
-    const Color(0xFFF9F871),
+    const Color(0xFF6263fb),
+    const Color(0xFFbb4fe4),
+    const Color(0xFFf037c4),
+    const Color(0xFFff2e9e),
+    const Color(0xFFff4377),
+    const Color(0xFFff6552),
+    const Color(0xFFff872f),
+    const Color (0xffffa600),
   ];
 
   int colorIndex = 0;
@@ -86,189 +111,143 @@ class _DataVisualizationPageState extends State<DataVisualizationPage> {
       appBar: AppBar(
         title: const Text('Data Visualization'),
         centerTitle: true,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Last Month'),
+            Tab(text: 'This Month'),
+          ],
+        ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          // color: Colors.yellow[100],
-          padding: const EdgeInsets.all(8.0),
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Total Expenses',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  // Current date
-                  Text(
-                    // Get current date but with month in form of text (April for example)
-                    // Also add the postfix for days (st, nd, rd, th)
-                    // Example: 1st January 2024
-                    '${DateTime.now().day}${getDayOfMonthSuffix(DateTime.now().day)} ${DateFormat.MMMM().format(DateTime.now())} ${DateTime.now().year}',
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-              StreamBuilder<Map<String, double>>(
-                stream: streamExpenses(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Padding(
-                      // Loading data process
-                      padding: const EdgeInsets.symmetric(vertical: 30),
-                      child: Center(
-                        child: SizedBox(
-                          width: 30,
-                          height: 30,
-                          child: CircularProgressIndicator(
-                            strokeWidth: 2, // Set the stroke width
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                              Theme.of(context)
-                                  .primaryColor, // Set your desired color
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Text('No data available');
-                  } else {
-                    // Display your UI using the fetched data
-                    final Map<String, double> expenseData = snapshot.data!;
-                    double total = getTotal(expenseData);
-                    categoryColors = expenseData.entries
-                        .map((e) => MapEntry(e.key, getNextColor()))
-                        .fold<Map<String, Color>>({}, (prev, element) {
-                      prev[element.key] = element.value;
-                      return prev;
-                    });
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          buildExpenseView(false), // Last month
+          buildExpenseView(true), // Current month
+        ],
+      ),
+    );
+  }
 
-                    return Column(
-                      children: [
-                        SizedBox.square(
-                          dimension: MediaQuery.of(context).size.width * 0.8,
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: displayPieChart(total, expenseData),
-                          ),
-                        ),
-                        Column(
-                            children: expenseData.entries
-                                .map((e) => Container(
-                                      margin: const EdgeInsets.symmetric(
-                                          vertical: 6.0),
-                                      child: expenseItem(e.key, e.value),
-                                    ))
-                                .toList()),
-                      ],
-                    );
-                  }
-                },
-              )
-            ],
-          ),
+  Widget buildExpenseView(bool isCurrentMonth) {
+    // Formatting date for display
+    var now = DateTime.now();
+    var displayedMonth =
+        isCurrentMonth ? now : DateTime(now.year, now.month - 1);
+    var monthFormatter = DateFormat('MMMM yyyy');
+
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              isCurrentMonth ? 'Expenses This Month' : 'Expenses Last Month',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              monthFormatter.format(displayedMonth),
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            StreamBuilder<Map<String, double>>(
+              stream: streamExpenses(isCurrentMonth),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Text('No data available');
+                } else {
+                  final expenseData = snapshot.data!;
+                  final total = getTotal(expenseData);
+
+                  return Column(
+                    children: [
+                      SizedBox(
+                        height: 200, // Adjust size as needed
+                        child: displayPieChart(total, expenseData),
+                      ),
+                      ...expenseData.entries
+                          .map((e) => expenseItem(e.key, e.value))
+                          .toList(),
+                    ],
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 
   Widget displayPieChart(double total, Map<String, double> expenseData) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Spent',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-              ),
+    // Constructing the categoryColors map
+    categoryColors = Map.fromIterable(
+      expenseData.keys,
+      key: (item) => item,
+      value: (item) => getNextColor(),
+    );
+
+    return PieChart(
+      PieChartData(
+        sections: expenseData.entries.map((entry) {
+          final category = entry.key;
+          final amount = entry.value;
+          final color = categoryColors[category]!;
+          final percentage = (amount / total) * 100;
+
+          return PieChartSectionData(
+            color: color,
+            value: amount,
+            title: '${percentage.toStringAsFixed(0)}%',
+            radius: 50,
+            titleStyle: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
-            Text(
-              '\$${total.toStringAsFixed(2)}',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-        PieChart(
-          swapAnimationDuration: const Duration(milliseconds: 500),
-          swapAnimationCurve: Curves.easeInOut,
-          PieChartData(
-            sections: expenseData.entries
-                .map((e) => PieChartSectionData(
-                      color: categoryColors[e.key],
-                      value: e.value / total,
-                      title: '${((e.value / total) * 100).toStringAsFixed(0)}%',
-                      radius: 50,
-                      titleStyle: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ))
-                .toList(),
-          ),
-        ),
-      ],
+          );
+        }).toList(),
+        centerSpaceRadius: 40,
+        sectionsSpace: 2,
+      ),
     );
   }
 
   Widget expenseItem(String category, double amount) {
-    const Color textColor = Colors.white;
+    final textColor = Colors.white;
+    final categoryColor = categoryColors[category] ?? Colors.grey;
+
     return Container(
       height: 60,
       padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.symmetric(vertical: 4.0),
       decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(8)),
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              darkenColorHSL(categoryColors[category]!),
-              lightenColorHSL(categoryColors[category]!),
-            ],
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: categoryColors[category]!.withAlpha(150),
-              blurRadius: 8,
-              offset: const Offset(0, 2),
-            ),
-          ]),
+        borderRadius: BorderRadius.circular(8),
+        color: categoryColor,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
             category,
-            style: const TextStyle(
-              color: textColor,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
+            style: TextStyle(
+                color: textColor, fontSize: 18, fontWeight: FontWeight.bold),
           ),
           Text(
             '\$${amount.toStringAsFixed(2)}',
-            style: const TextStyle(
-              color: textColor,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
+            style: TextStyle(
+                color: textColor, fontSize: 18, fontWeight: FontWeight.bold),
           ),
         ],
       ),
